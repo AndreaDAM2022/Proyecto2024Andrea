7fad1f1f6115ec42e165f4f2ff1afd3445805138
AndreaDAM2022 <106146174+AndreaDAM2022@users.noreply.github.com>
2024-02-02 18:33:23 +0000

    Update Main

--- app/src/main/java/com/example/andrea_proyecto/MainActivity.kt
+++ app/src/main/java/com/example/andrea_proyecto/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.andrea_proyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val username = findViewById<EditText>(R.id.editTextUsername)
+        val password = findViewById<EditText>(R.id.editTextPassword)
+        val butnRegistro  = findViewById<Button>(R.id.buttonRegister)
+
+        butnRegistro.setOnClickListener(){
+            if (username.text.toString() == password.text.toString()){ //El "toString" lo utilizamos para poder acceder mediante números y caracteres
+                val intent = Intent(this@MainActivity, Formulario::class.java)
+                startActivity(intent)
+            } else{
+                Toast.makeText(this, "Usuario y Contraseña no válidos", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
